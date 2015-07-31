d3f6e106811e5d80607df1300f67a67a23fb2b70
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-07-31 17:48:23 -0300

    Bump version to 0.0.3, update README title and correct some mistakes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Simple Node Boilerplate
+# Peer Hub
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
