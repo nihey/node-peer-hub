cf17e5732ed02b26365543254287cd369523f049
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-07-31 17:42:16 -0300

    Basic signalling

---
+++ bin/index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('join', function(room) {
+      // Get the list of peers in the room
+      var peers = Object.keys(io.nsps['/'].adapter.rooms['foobar'] || {});
+      // Send them to the client
+      socket.emit('peers', peers);
+      // And then add the client to the room
+      socket.join(room);
+    });
+
+    socket.on('signal', function(data) {
+      var client = io.sockets.connected[data.id];
+      client && client.emit('signal', {
+        id: socket.id,
+        signal: data.signal,
+      });
+    });
+
+    socket.on('disconnect', function() {
+      console.log('a user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
